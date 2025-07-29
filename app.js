6d14e72fa6a9d0b9672d1da7866af593014aedaf
MADRIDBOI\17769 <1776931339@qq.com>
2025-07-29 11:00:06 +0800

    secont-conmmit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import chatRouter from './routes/chat.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3008;
 
 // ✅ 允许跨域请求
 app.use(cors());
